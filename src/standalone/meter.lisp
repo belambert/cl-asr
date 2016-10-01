efee7e952e2a4ee195e3208f751d772324d324ff
Ben Lambert <ben@benjaminlambert.com>
2016-10-01 18:33:33 -0400

    Updating interactive meter.

--- src/standalone/meter.lisp
+++ src/standalone/meter.lisp
@@ -5,24 +5,25 @@
 
 ;;;; The display portion of a little text line-level meter.  Currently only working properly in *inferior-lisp* and on the command line
 
-(defun display-meter (x &key (max 50) (width 50))
+(defun display-meter (x &key (width 50))
   "Display the intial meter."
-  (let ((delta (/ max width)))    
-    (setf x (floor (* x delta)))
-    (format t "delta=~a, x=~A, width=~A~%" delta x width)
-    (format t "|~A~A|" (make-string x :initial-element #\*) (make-string (- width x) :initial-element #\_))
-    (force-output)))
+  (when (> x 1) (setf x 1))
+  (when (< x 0) (setf x 0))
+  (setf x (floor (* x width))) 
+  (format t "|~A~A| (~a)" (make-string x :initial-element #\*) (make-string (- width x) :initial-element #\_) x)
+  (terpri)
+  (force-output)))
 
-(defun update-meter (x &key (max 50) (width 50))
+(defun update-meter (x &key (width 50))
   "Update the meter to a new level, without going to a new line."
   (format t "~c" #\Return);; go to the beginning of the line
-  (display-meter x :max max :width width))
+  (display-meter x :width width))
 
-(defun test-meter (&key (n 100) (count 10))
+(defun test-meter (&key (count 10))
   "Randomly generate numbers and display them on the meter to test it."
-  (display-meter 0 :max n)
+  (display-meter 0)
   (dotimes (i count)
-    (update-meter (random n) :max n)
+    (update-meter (random 1.0))
     (sleep 1)))
 
 #+port-audio
@@ -30,16 +31,14 @@
   "This is supposed to periodically output the total line level meter."
   (let ((segments (ceiling (/ seconds update-freq)))
 	(samples-per-segment (truncate (* sample-rate update-freq)))
-	(max-volume 1000))
-    (display-meter 0 :max max-volume)
+	(max-volume 100))
+    (display-meter 0)
     (dotimes (i segments)
       (handler-case
 	  (let* ((samples (port-audio:read-audio-samples samples-per-segment))	;; 22k is about 1/2 a second...
 		 (sum (reduce (lambda (x y) (+ x (abs y))) samples))
 		 (level (/ sum samples-per-segment)))
-	    ;;(update-meter level :max max-volume)
-	    ;;(update-meter sum :max max-volume)
-	    (format t "Sum:   ~A~%" sum)
-	    (format t "Level: ~A~%" level))
+	    (declare (ignore level))
+	    (update-meter (/ sum max-volume)))
 	(error (e) (when verbose (format t "Skipping segment... ~A~%" e))))
       (force-output t))))
